838f9a32041660b3345365d654bc2660ee9f8bca
Anuragupadhayay <anuragu901@gmail.com>
2025-07-26 12:47:16 +0530

    added the card component

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card(){
+    return (
+        <div>
+            <img class="w-24 h-24 md:w-48 md:h-auto md: rounded-none max-auto" src="" alt="" width="384" height="512"/>
+            <h1 className="text-2xl bg-green-500 p-3 rounded">A card for photos</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, praesentium!</p>
+        </div>
+    )
+}
+
+export default Card
