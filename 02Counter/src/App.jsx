abc73571fed3d430bcdab261588038e1fbe5dd39
Anuragupadhayay <anuragu901@gmail.com>
2025-07-26 11:30:19 +0530

    Learned State in react

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -4,18 +4,30 @@ import './App.css'
 
 function App() {
 
-
-  let counter = 15
+  const [counter, setCounter] = useState(15)  //in the array the first element is variable and the second element is method
+  // let counter = 15
 
 
   const AddValue = () => {
-    counter+=1
-    console.log(counter);    
+      setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // this will not update the values in the website, if you call this function it will only update it to +1,
+    //this is because of reconciliation algorithm in the react that batches all the methods in single method
+    //but what if you want it this way? here's the solution you can use, it is called callback method
+
+     setCounter((prevCounter) => prevCounter + 1)
+    // setCounter((prevCounter) => prevCounter + 1)
+    // setCounter((prevCounter) => prevCounter + 1)
+    // setCounter((prevCounter) => prevCounter + 1)
+
+
   }
 
   const RemoveValue = () => {
-    counter-=1
-    console.log(counter);    
+    setCounter(counter - 1) 
+    setCounter((prevCounter) => prevCounter - 1)   
   }
   return (
     <>
