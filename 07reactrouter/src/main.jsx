c9353d1ac2dab4558abbe2630a10318e34d5618c
Anuragupadhayay <anuragu901@gmail.com>
2025-08-15 13:04:21 +0530

    imported both home and about component in the main file

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Layout from './layouts.jsx';
+import About from './components/About/About.jsx';
+import Home from './components/Home/Home.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path=''/>
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About/>}/>
     </Route>
   )
 );
